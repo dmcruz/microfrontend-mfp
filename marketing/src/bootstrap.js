30780f3c961766e72e9ee038cba1a50cf37492e2
Donna <dmcruz@gmail.com>
2021-12-02 22:17:06 +0800

    multi-tier navigation (router)

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // mount function to start the app
-const mount=  (el) => {
+const mount=  (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
   ReactDOM.render(
-    <App />,
+    <App history={history} />,
     el
-  )
+  );
+
+  return {
+    onParentNavigate({ pathname: nextPathname  }) {
+      console.log('Container just navigated');
+      const { pathname } = history.location;
+      if(pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  }
 };
 // if in development call mount immediately
 
@@ -15,7 +32,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if(devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 // we are running through container and we should export the mount function
